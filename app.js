78b7773d3c1442e4ee3824ff5575e582603cede0
Maartje <amschreijenberg@gmail.com>
2021-06-16 15:42:28 +0200

    added some extra exceeds code

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const alertBanner = document.getElementById("alert");
+const notifications = document.getElementById("notifications");
+const popup = document.getElementById("popup");
+const dot = document.getElementById("dot");
+const user= document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
 
 alertBanner.innerHTML =
 `<div class="alert-banner">
@@ -12,3 +19,45 @@ alertBanner.addEventListener('click', e =>{
         alertBanner.style.display ="none"
     }
 });
+
+send.addEventListener('click', ()=>{
+    if(user.value === "" && message.value=== ""){
+        alert("Please fill out user and messages fields before sending");
+    }else if(user.value === ""){
+        alert("Please fill out user field before sending");
+    }else if(message.value === ""){
+        alert("Please fill out message field before sending");
+    }else{
+        alert(`Message succesfully sent to: ${user.value}`)
+    }
+} );
+
+let messageShown=false;
+notifications.addEventListener('click', ()=>{
+
+    if(!messageShown){
+        let message = document.createElement('div');
+        message.className = "popup-message";
+        message.innerHTML =
+        `
+        <p>You have a message from Dawn</p>
+        <p class="message-close">X</p>   
+        `;
+        popup.appendChild(message);
+        dot.style.display = "none";
+        messageShown=true;
+    }
+  
+});
+
+popup.addEventListener('click', e =>{
+    const element = e.target;
+    
+    if(element.classList.contains("message-close")){
+        const parent = element.parentNode;
+        parent.parentNode.removeChild(parent);
+    }
+});
+
+
+
