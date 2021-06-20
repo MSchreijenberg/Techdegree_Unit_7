0dcc9102d8dae55fd35a6a6f58541bebd8066912
Maartje <amschreijenberg@gmail.com>
2021-06-20 12:46:35 +0200

    added js for exceeds

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const popup = document.getElementById("popup");
 const dot = document.getElementById("dot");
 const user= document.getElementById("userField");
 const message = document.getElementById("messageField");
-const send = document.getElementById("send");
-
+const form = document.getElementById("form");
+let trafficNav = document.querySelector(".traffic-nav");
 
 alertBanner.innerHTML =
 `<div class="alert-banner">
@@ -20,23 +20,30 @@ alertBanner.addEventListener('click', e =>{
     }
 });
 
-send.addEventListener('click', ()=>{
+form.addEventListener('submit', e =>{
+    //let userName = user.options[user.selectedIndex].text;
+    console.log(user.value);
     if(user.value === "" && message.value=== ""){
         alert("Please fill out user and messages fields before sending");
+        e.preventDefault();
     }else if(user.value === ""){
         alert("Please fill out user field before sending");
+        e.preventDefault();
     }else if(message.value === ""){
         alert("Please fill out message field before sending");
+        e.preventDefault();
     }else{
         alert(`Message succesfully sent to: ${user.value}`)
     }
 } );
 
-let messageShown=false;
-notifications.addEventListener('click', ()=>{
+//create messages, but do not show
+let messageShown;
 
-    if(!messageShown){
-        let message = document.createElement('div');
+function createMessages(){
+    if(popup.childNodes.length==0 && dot.style.display !== "none"){
+        for(let i=0; i<2; i++){
+            let message = document.createElement('div');
         message.className = "popup-message";
         message.innerHTML =
         `
@@ -44,10 +51,29 @@ notifications.addEventListener('click', ()=>{
         <p class="message-close">X</p>   
         `;
         popup.appendChild(message);
-        dot.style.display = "none";
-        messageShown=true;
+        }
     }
-  
+    popup.style.display = "none";
+}
+createMessages();
+
+
+notifications.addEventListener('click', e =>{
+    const element = e.target;
+    //delete dot, when messages show for first time
+    dot.style.display = "none";
+    //only clicking on messages or bell should hide/show messages
+    if(element.className!=="message-close"){
+        if(!messageShown){
+            popup.style.display = "block";
+            messageShown=true;
+        }
+        else{
+            popup.style.display = "none";
+            messageShown= false;
+        }
+            
+    }
 });
 
 popup.addEventListener('click', e =>{
@@ -58,6 +84,67 @@ popup.addEventListener('click', e =>{
         parent.parentNode.removeChild(parent);
     }
 });
+
+trafficNav.addEventListener('click', e =>{
+    const element = e.target;  
+    const datatype = element.textContent;    
+    if(element.className ===  "traffic-nav-link" || element.className ===  "traffic-nav-link active"){
+        //set active button inactive
+        let lis = document.querySelectorAll(".traffic-nav-link");
+        for(let i=0; i<lis.length; i++){
+            let activeButton = lis[i];
+            if(activeButton.className==="traffic-nav-link active"){
+                activeButton.className="traffic-nav-link"
+            }
+        }
+        //set clicked button to active if not already active
+        if(element.className==="traffic-nav-link"){
+         element.className = "traffic-nav-link active";
+        }
+      
+        //change graph
+            updateTrafficData(datatype);
+            updateChart(trafficChart, trafficData);
+        }
+    }  
+);
+
+//saving settings to localStorage
+const emailCheckbox = document.getElementById("emailSettings");
+const publicCheckbox = document.getElementById("publicSettings");
+const timeZone = document.getElementById("timezone");
+const saveButton = document.getElementById("save");
+const cancelButton = document.getElementById("cancel");
+
+//set default settings
+if(localStorage.length != 0){
+   emailCheckbox.checked = localStorage.getItem('emailNotificationsOn');
+   publicCheckbox.checked = localStorage.getItem('profilePublic');
+   timeZone.value = localStorage.timeZone    
+}
+
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('emailNotificationsOn', emailCheckbox.checked );
+    localStorage.setItem('profilePublic', publicCheckbox.checked);
+    localStorage.setItem('timeZone', timeZone.value );
+});
+
+cancelButton.addEventListener('click', ()=>{
+    emailCheckbox.checked=false;
+    publicCheckbox.checked=false;
+    timeZone.value = 'default';
+    localStorage.clear();
+    location.reload();
+   
+});
+
+
+
+
+
+
+
 
 
 
--- charts.js
+++ charts.js
@@ -1,13 +1,28 @@
-//charts
+//traffic data:
+let hourlyLabels =['0:00','1:00','2:00','3:00','4:00','5:00','6:00',
+'7:00','8:00','9:00','10:00'];
+let hourlyData = [20, 500, 2000, 1740, 1500, 600, 700, 400, 100, 2000, 100];
+
+let dailyLabels =['16','17','18`','19','20','21','22',
+'23','24','25','26'];
+let dailyData = [2025, 570, 1800, 1900, 1650, 800, 1000, 1250, 1000, 1500, 999];
+
+let weeklyLabels =['16-22','23-29','30-5','6-12','13-19','20-26','27-3',
+'4-10','11-17','18-24','25-31'];
+let weeklyData = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500];
+
+let monthlyLabels =['Feb','Mar','Apr','May','Jun','Jul','Aug',
+'Sep','Oct','Nov','Dec'];
+let monthlyData = [1000, 1550, 1200, 2100, 1560, 1850, 950, 1950, 1600, 1500, 2500];
+
 
 //traffic chart
 let trafficCanvas = document.getElementById('traffic-chart');
 
 let trafficData = {
-    labels: ['16-22','23-29','30-5','6-12','13-19','20-26','27-3',
-    '4-10','11-17','18-24','25-31'],
+    labels: weeklyLabels,
     datasets: [{
-      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      data: weeklyData,
       backgroundColor: 'rgba(116, 119, 191, .3)',
       borderWidth: 1,
       borderColor: 'rgba(112, 127, 194, .3)', 
@@ -23,9 +38,6 @@ let trafficData = {
 
  let trafficOptions ={
      aspectRatio: 1.9,
-     animation: {
-         duration: 0
-     },
      scales: {
          y: {beginAtZero: true
      }
@@ -37,16 +49,44 @@ let trafficData = {
         }
     }
 }
-
-let trafficChart = new Chart(trafficCanvas,{
+   //make first chart
+    let trafficChart = new Chart(trafficCanvas,{
     type: 'line',
     data: trafficData,
     options: trafficOptions
   });
 
+  function updateTrafficData(datatype){
+      if(datatype === "Hourly"){
+         trafficData.datasets[0].data = hourlyData;
+         trafficData.labels = hourlyLabels;          
+      }else if(datatype === "Daily"){
+        trafficData.datasets[0].data = dailyData;  
+        trafficData.labels = dailyLabels;  
+      }else if(datatype ==="Weekly"){
+        trafficData.datasets[0].data = weeklyData;
+        trafficData.labels = weeklyLabels; 
+      }else if(datatype === "Monthly"){
+        trafficData.datasets[0].data = monthlyData;
+        trafficData.labels = monthlyLabels; 
+      }
+      updateChart(trafficChart, trafficData)
+ }
+
+
+  function updateChart(chart, data){
+    chart.data.datasets[0].data = data.datasets[0].data;  
+    chart.data.labels = data.labels;
+    chart.update({
+        duration: 1000,
+        easing: 'linear',
+    });
+  };
+
+
  //daily chart
   const dailyCanvas = document.getElementById('daily-chart');
-  const dailyData= {
+  const dailyTrafficData= {
       labels: ["S", "M", "T", "W", "T", "F", "S"],
       datasets: [{
           label: '# of Hits',
@@ -72,7 +112,7 @@ let trafficChart = new Chart(trafficCanvas,{
 
   let dailyChart = new Chart(dailyCanvas, {
       type: 'bar',
-      data: dailyData,
+      data: dailyTrafficData,
       options: dailyOptions
   });
 
